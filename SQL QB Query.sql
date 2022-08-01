fd1df8636ad1ec2cfbb51916b8fd9087c7707f13
jasonmishkin <82347404+jasonmishkin@users.noreply.github.com>
2022-08-01 14:42:48 -0400

    Update SQL QB Query.sql

--- SQL QB Query.sql
+++ SQL QB Query.sql
@@ -1,25 +1,16 @@
-select top (27) player, team, pass_yds, Pass_TD,INT,Rush_YDS,Rush_TD, Fantasy_Points_Per_Game
-FROM [NFL Fantasy].[dbo].[QBstats]
-where Pass_YDS > 1500
-order by Fantasy_Points_Per_Game desc
-
-select top (10) player,team, pos, Rush_YDS, Rush_TD
-FROM [NFL Fantasy].[dbo].[QBstats]
-order by Rush_TD desc
-
-select top (22) player,team, pos, Pass_YDS, Pass_TD, INT,round(Pass_TD/int,2) as 'TD/INT Ratio'
-FROM [NFL Fantasy].[dbo].[QBstats]
-where INT != 0 and Pass_YDS > 2000 
-order by [TD/INT Ratio] desc
-
-select top (10) player,team, pos, Pct_Owned
-FROM [NFL Fantasy].[dbo].[QBstats]
-where pos = 'QB-R'
-
-select top (10) player, team, Fumbles_Lost, SACKS
-FROM [NFL Fantasy].[dbo].[QBstats]
-order by SACKS desc
-
-
-
+create or replace view  qb_view as
+select *,
+case when team in ('(NYJ) ','(BUF) ','(MIA) ',' (NE) ') then 'AFC East'
+	 when team 
+     in ('(CIN) ','(BAL) ','(PIT) ','(CLE) ') then 'AFC North'
+     when team in ('(TEN) ','(IND) ','(JAC) ','(HOU) ') then 'AFC South'
+     when team in ('(LAC) ',' (KC) ',' (LV) ','(DEN) ') then 'AFC West'
+     when team in ('(DAL) ','(PHI) ','(WAS) ','(NYG) ') then 'NFC East'
+     when team in (' (GB) ','(MIN) ','(DET) ','(CHI) ') then 'NFC North'
+     when team in (' (TB) ','(ATL) ','(CAR) ',' (NO) ') then 'NFC South'
+     when team in ('(LAR) ','(ARI) ',' (SF) ','(SEA) ') then 'NFC West'
+	else 'Free Agent'
+    end as 'Division',
+    round(pass_td/inter,2) as 'TD/Int Ratio'
+    from qbstats;
 
